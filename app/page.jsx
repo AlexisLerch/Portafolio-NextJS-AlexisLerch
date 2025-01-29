e97f0d9c56036e688bc7b85420eb9498a23d71df
Alexis Lerch <alexis.lerch123@gmail.com>
2025-01-29 10:05:33 -0300

    Correccion de bug al descargar cv

--- app/page.jsx
+++ app/page.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
             {/* boton y social */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <a href="../public/assets1" download='AlexisLerch-cv'>
+              <a href="/assets1/AlexisLerch-cv.pdf" target='_blank' download='AlexisLerch-cv.pdf'>
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                 <span>Descargar CV</span>
                 <FiDownload className="text-xl" />
