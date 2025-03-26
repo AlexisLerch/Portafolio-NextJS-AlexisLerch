ba154dca4a1e8027d85194e3192d939d12d90850
Alexis Lerch <alexis.lerch123@gmail.com>
2025-03-26 14:10:22 -0300

    Foto

--- app/page.jsx
+++ app/page.jsx
@@ -46,9 +46,9 @@ const Home = () => {
           </div>
 
           {/* photo */}
-          {/* <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
-          </div> */}
+          </div>
         </div>
       </div>
         
